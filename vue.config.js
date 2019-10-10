3716cff77170d01d8da397a70d00f8ef6158df59
luoming <1661391985@qq.com>
2019-10-11 00:11:16 +0800

    first

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    port: 8080, // 端口号
+    open: true, //配置自动启动浏览器
+    proxy: {
+      "/api": {
+        target: "http://www.baidu.com",
+        changeOrigin: true
+      },
+    }
+  },
 };
